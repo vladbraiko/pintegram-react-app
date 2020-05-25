d360a1abbb215140d8873d40d114e1a9eaba92a8
Vladyslav Braiko <vladyslavbar@gmail.com>
2020-05-25 17:04:31 +0300

    Updates

---
+++ precache-manifest.dde12da08a70ef28e7cd47b21a4d2a42.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54feadf81984faab52632edd1879c017",
+    "url": "/pintegram-react-app/index.html"
+  },
+  {
+    "revision": "161bf63fbfc39f14a8e6",
+    "url": "/pintegram-react-app/static/css/2.33ece8ec.chunk.css"
+  },
+  {
+    "revision": "c04b509c7af9d44f27c3",
+    "url": "/pintegram-react-app/static/css/main.2d40a99e.chunk.css"
+  },
+  {
+    "revision": "161bf63fbfc39f14a8e6",
+    "url": "/pintegram-react-app/static/js/2.de11bbe6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pintegram-react-app/static/js/2.de11bbe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04b509c7af9d44f27c3",
+    "url": "/pintegram-react-app/static/js/main.9f7422bb.chunk.js"
+  },
+  {
+    "revision": "7865cb7d9a226514ba6a",
+    "url": "/pintegram-react-app/static/js/runtime-main.6af5cb22.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/pintegram-react-app/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/pintegram-react-app/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/pintegram-react-app/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/pintegram-react-app/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
